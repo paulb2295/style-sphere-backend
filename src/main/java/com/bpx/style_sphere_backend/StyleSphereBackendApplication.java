a810be0585c0870ab2b118232e4a07d6d846eafa
paulb2295 <borleapaul@gmail.com>
2025-01-15 21:59:15 +0200

    first commit

---
+++ src/main/java/com/bpx/style_sphere_backend/StyleSphereBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bpx.style_sphere_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StyleSphereBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StyleSphereBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bpx/style_sphere_backend/StyleSphereBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpx.style_sphere_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StyleSphereBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
